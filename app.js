4e0a47230c9a3bb45d78ce89b8e31b24ab24bcdf
anback <anders@swedishtravelmafia.com>
2014-05-07 11:55:17 +0200

    fixed error with package.json

--- app.js
+++ app.js
@@ -5,14 +5,12 @@ var app = function() {
 	var childProcess = require('child_process');
 	var phantomjs = require('phantomjs');
 	var MongoClient = require('mongodb').MongoClient;
-	var binPath = phantomjs.path
+	var binPath = phantomjs.path;
 	var url = 'http://www.momondo.se/multicity/?Search=true&TripType=return&SegNo=2&SO0=STO&SD0=ROM&SDP0=25-06-2014&SO1=ROM&SD1=STO&SDP1=31-07-2014&AD=1&TK=ECO&DO=false&NA=false';
 	var mongoUrl = 'mongodb://127.0.0.1:27017/test';
-	var childArgs = [
-	  path.join(__dirname, 'phantomjs-momondo-scraper.js'), url
-	]
+	var childArgs = [path.join(__dirname, 'phantomjs-momondo-scraper.js'), url];
 
-	var url = "";
+	var url = '';
 
 	console.log("Start Momondo Search ..");
 	childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
@@ -30,7 +28,6 @@ var app = function() {
 			console.log("Getting data from: " + url);
 			request(url, function (error, response, body) {
 			  if (!error && response.statusCode == 200) {
-
 				console.log("Parsing data ..");
 			  	var json = JSON.parse(body);
 
@@ -50,8 +47,3 @@ var app = function() {
 }
 
 app();
-
-
-
-
-
