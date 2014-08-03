a30d625316eff9afdb4aeb478de349ef0867ff6f
anback <anders@swedishtravelmafia.com>
2014-08-03 17:58:59 +0200

    first commit

--- app.js
+++ app.js
@@ -1,49 +1,41 @@
-var app = function() {
-	var path = require('path');
-	var request = require('request');
-	var fs = require('fs');
-	var childProcess = require('child_process');
-	var phantomjs = require('phantomjs');
-	var MongoClient = require('mongodb').MongoClient;
-	var binPath = phantomjs.path;
-	var url = 'http://www.momondo.se/multicity/?Search=true&TripType=return&SegNo=2&SO0=STO&SD0=ROM&SDP0=25-06-2014&SO1=ROM&SD1=STO&SDP1=31-07-2014&AD=1&TK=ECO&DO=false&NA=false';
-	var mongoUrl = 'mongodb://127.0.0.1:27017/test';
-	var childArgs = [path.join(__dirname, 'phantomjs-momondo-scraper.js'), url];
+var exec = require('child_process').exec;
+var skyscannerScraperCommand = 'java -jar ' + require('path').join(__dirname, 'SSScraper.jar ');
+var mubsub = require('mubsub');
 
-	var url = '';
-
-	console.log("Start Momondo Search ..");
-	childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
-		console.log("Url for result data obtained ..");
-		var splits = stdout.split('\n');
-		splits.forEach(function(item, index) {
-			if(item.indexOf("http://www.momondo.se/api/3.0/FlightSearch/") != -1)
-				url = item;
-		});
-
-		var uniqueString = new Date().getTime().toString();
-
-		console.log("Wating for 10 seconds")
-		setTimeout(function() {
-			console.log("Getting data from: " + url);
-			request(url, function (error, response, body) {
-			  if (!error && response.statusCode == 200) {
-				console.log("Parsing data ..");
-			  	var json = JSON.parse(body);
-
-			  	console.log("Connecting to Mongo ..");
-				MongoClient.connect(mongoUrl, function(err, db) {
-				    if(err) throw err;
-				    console.log("Inserting document .. ")
-				    var collection = db.collection('test_insert');
-				    collection.insert(json, function(err, docs) {
-				    	console.log("Finished Inserting");
-				    });
-				  });
-			  }
-			});
-		}, 10000);
-	});
+var getFormattedDate = function(input) {
+    input = input.toJSON().split('T')[0];
+    var parts = input.split('-');
+    var date = new Date(parts[0], parts[1]-1, parts[2]); // Note: months are 0-based
+    return date.getDate() + '.' + (parseInt(date.getMonth()) + 1) + '.' + date.getFullYear();
 }
 
-app();
+var executeSearch = function(data) {
+
+    if(data.in_FromCity == undefined)
+        return
+    if(data.in_ToCity == undefined)
+        return
+    if(data.in_DepartureDate == undefined)
+        return
+    if(data.in_ReturnDate == undefined)
+        return
+
+    //Write some code that executes the runnable jar file
+    var args=[data.in_FromCity, data.in_ToCity, getFormattedDate(data.in_DepartureDate), getFormattedDate(data.in_ReturnDate)];
+    console.log(skyscannerScraperCommand + args.join(' '));
+    exec(skyscannerScraperCommand + args.join(' '), function callback(error, stdout, stderr){
+        console.log(error);
+        console.log(stdout);
+        console.log(stderr);
+    });
+}
+
+var client = mubsub('mongodb://swalo:84kAanan@ds051658.mongolab.com:51658/swalo');
+var channel = client.channel('searches');
+
+client.on('error', console.error);
+channel.on('error', console.error);
+
+channel.subscribe('NewSearch', function (message) {
+    executeSearch(message);
+});
