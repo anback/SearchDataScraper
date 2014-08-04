72b602d6a9967e931be9deab309f2065b806827f
anback <anders@swedishtravelmafia.com>
2014-08-04 14:00:27 +0200

    changed to phantomjs

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-var exec = require('child_process').exec;
-var skyscannerScraperCommand = 'java -jar ' + require('path').join(__dirname, 'SSScraper.jar ');
+//var exec = require('child_process').exec;
+//var skyscannerScraperCommand = 'java -jar ' + require('path').join(__dirname, 'SSScraper.jar ');
 var mubsub = require('mubsub');
 var isdebug = process.argv[2] == 'debug';
+var phantomjs = require('phantomjs');
+var binPath = phantomjs.path;
+var path = require('path');
+var childProcess = require('child_process');
 console.log("IsDebug: " + isdebug);
 
+
 var getFormattedDate = function(input) {
-    input = input.toJSON().split('T')[0];
-    var parts = input.split('-');
-    var date = new Date(parts[0], parts[1]-1, parts[2]); // Note: months are 0-based
-    return date.getDate() + '.' + (parseInt(date.getMonth()) + 1) + '.' + date.getFullYear();
+    input = input.toJSON().split('20')[1];
+    input = input.split('T')[0];
+    input = input.replace('-','');
+    input = input.replace('-','');
+    return input;
+    
 }
 
 var executeSearch = function(data) {
@@ -23,10 +30,20 @@ var executeSearch = function(data) {
         return
 
     //Write some code that executes the runnable jar file
-    var args=[data.in_FromCity, data.in_ToCity, getFormattedDate(data.in_DepartureDate), getFormattedDate(data.in_ReturnDate)];
+    //var args=[data.in_FromCity, data.in_ToCity, getFormattedDate(data.in_DepartureDate), getFormattedDate(data.in_ReturnDate)];
+    var ssUrl = 'http://www.skyscanner.de/transport/fluge/{0}/{1}/{2}/{3}/?usrplace=DE'
+    ssUrl = ssUrl.format(data.in_FromCity, data.in_ToCity, getFormattedDate(data.in_DepartureDate), getFormattedDate(data.in_ReturnDate));
+    console.log(ssUrl);
+    /*
     console.log(skyscannerScraperCommand + args.join(' '));
     exec(skyscannerScraperCommand + args.join(' '), function callback(error, stdout, stderr){
-        console.log(error);
+
+    });*/
+
+    //console.log(path.join(__dirname, 'ssScraper.js'));
+    childProcess.execFile(binPath, [path.join(__dirname, 'ssScraper.js'), ssUrl], function(err, stdout, stderr) {
+        console.log("hej");
+        console.log(err);
         console.log(stdout);
         console.log(stderr);
     });
@@ -41,7 +58,21 @@ channel.on('error', console.error);
 var count = 0;
 channel.subscribe('NewSearch', function (message) {
 
-    if(!isdebug || count == 0)
+    //if(!isdebug || count == 0)
         executeSearch(message);
-    count++;
+    //count++;
 });
+
+
+// First, checks if it isn't implemented yet.
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) { 
+      return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+      ;
+    });
+  };
+}
