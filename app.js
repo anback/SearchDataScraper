20378f51a9e357c6a75b607b4bfbc450d8491ef7
anback <anders@swedishtravelmafia.com>
2014-08-05 20:25:58 +0200

    removed json parsing

--- app.js
+++ app.js
@@ -31,18 +31,14 @@ var executeSearch = function(data) {
     ssUrl = ssUrl.format(data.in_FromCity, data.in_ToCity, getFormattedDate(data.in_DepartureDate), getFormattedDate(data.in_ReturnDate));
     console.log(ssUrl);
     childProcess.execFile(binPath, [path.join(__dirname, 'ssScraper.js'), ssUrl], function(err, stdout, stderr) {
-        if(stdout.split(urlPrefix).length <= 1)
-            return;
-
-        var url = urlPrefix + stdout.split(urlPrefix)[1].split('?')[0];
-        console.log('publishing url: ' +  url);
-        urlChannel.publish('NewUrl', url);
+        var res = parseSSHTML(stdout);
+        
     });
 }
 
 var client = mubsub('mongodb://swalo:84kAanan@ds051658.mongolab.com:51658/swalo');
 var searchChannel = client.channel('searches');
-var urlChannel = client.channel('urls');
+var searchdata = client.channel('searchdata');
 
 client.on('error', console.error);
 searchChannel.on('error', console.error);
@@ -53,6 +49,54 @@ searchChannel.subscribe('NewSearch', function (message) {
         executeSearch(message);
     count = count + 1;
 });
+
+var parseAndSendSSHTML = function(html) {
+    var env = require('jsdom').env;
+    console.log(html);
+    // first argument can be html string, filename, or url
+    var res = {};
+    env(html, function (errors, window) {
+        var $ = require('jquery')(window);
+        console.log("best price: ");
+        console.log($('.header-info-bestprice').text());
+
+        res.BestPrice = $('.header-info-bestprice .price').text();
+        res.Itineraries = $('.day-list-item').map(function(item) {
+            $this = $(this);
+            var res = {};
+
+            res.CarrierIds = $('.carrier-wrapper img', $this).attr('data-carrier-ids');
+            res.OutboundLegDepartureTime = $('.leg.depart.flight .leg-flight-depart .leg-flight-time', $this).text();
+            res.InboundLegDepartureTime = $('.leg.return.flight .leg-flight-return .leg-flight-time', $this).text();
+            res.Origin = $('.leg.depart.flight .leg-flight-depart .leg-flight-station', $this).text();
+            res.Dest = $('.leg.return.flight .leg-flight-depart .leg-flight-station', $this).text();
+            res.prices = [];
+
+            res.prices = $('.details-group-altquotes-wrapper li a', $this).map(function() {
+                $that = $(this);
+                return {
+                    Price : $that.text().trim().split(' ')[1],
+                    OTA: $that.text().trim().split(' ')[0]
+                }
+            });
+
+            res.prices = $.makeArray( res.prices );
+
+            res.prices.unshift({
+                Price : $('.mainquote-wrapper a.mainquote-price', $this).text(),
+                OTA : $('.mainquote-wrapper a.mainquote-agent', $this).text(),
+            });
+
+            return res;
+        });
+        res.Itineraries = $.makeArray( res.Itineraries );
+        console.log("Publishing Result!");
+        urlChannel.publish('NewRes', res);
+    });
+    //console.log(res);
+    return res;
+}
+
 
 // First, checks if it isn't implemented yet.
 if (!String.prototype.format) {
